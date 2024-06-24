84380e6df1c1a33cc5b240e1510b3c14149515f1
Jake Lee <freejak5520@gmail.com>
2024-06-24 15:48:10 +0900

    Add Horizontal Scroll Component Example

--- src/components/horizontal-scroll/index.tsx
+++ src/components/horizontal-scroll/index.tsx
@@ -1,20 +1,17 @@
-export default function HorizontalScroll() {
+export default function HorizontalScroll({ children }: { children: React.ReactNode }) {
   return <div className="max-w-full overflow-x-scroll bg-gray-50 p-2 grid grid-flow-col gap-2">
-    <div className="w-16 h-16 bg-red-100"></div>
-    <div className="w-16 h-16 bg-red-100"></div>
-    <div className="w-16 h-16 bg-red-100"></div>
-    <div className="w-16 h-16 bg-red-100"></div>
-    <div className="w-16 h-16 bg-red-100"></div>
-    <div className="w-16 h-16 bg-red-100"></div>
-    <div className="w-16 h-16 bg-red-100"></div>
-    <div className="w-16 h-16 bg-red-100"></div>
-    <div className="w-16 h-16 bg-red-100"></div>
-    <div className="w-16 h-16 bg-red-100"></div>
-    <div className="w-16 h-16 bg-red-100"></div>
-    <div className="w-16 h-16 bg-red-100"></div>
-    <div className="w-16 h-16 bg-red-100"></div>
-    <div className="w-16 h-16 bg-red-100"></div>
-    <div className="w-16 h-16 bg-red-100"></div>
-    <div className="w-16 h-16 bg-red-100"></div>
+    {children}
   </div>
+}
+
+function HorizontalScrollItemExample({ children }: { children: React.ReactNode }) {
+  return <div className="w-32 h-32 flex justify-center items-center bg-blue-100">{children}</div>;
+}
+
+export function HorizontalScrollExample() {
+  const numList = new Array(10).fill(0).map((_, index) => index + 1);
+
+  return <HorizontalScroll>
+    {numList.map((num) => <HorizontalScrollItemExample key={num}>{num}</HorizontalScrollItemExample>)}
+  </HorizontalScroll>
 }
