0241557e5df9c4ca25921500ab3067bf8ffdf2d3
Jake Lee <freejak5520@gmail.com>
2024-06-24 15:19:31 +0900

    가로 스크롤 컴포넌트 예제

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
+import HorizontalScroll from './components/horizontal-scroll'
 
 function App() {
 
   return (
-    <>
+    <div className='max-w-screen-lg gap-4 flex flex-col items-center mx-auto'>
       <h1>Horizontal scroll</h1>
-    </>
+      <HorizontalScroll />
+    </div>
   )
 }
 
---
+++ src/components/horizontal-scroll/index.tsx
@@ -0,0 +1,20 @@
+export default function HorizontalScroll() {
+  return <div className="max-w-full overflow-x-scroll bg-gray-50 p-2 grid grid-flow-col gap-2">
+    <div className="w-16 h-16 bg-red-100"></div>
+    <div className="w-16 h-16 bg-red-100"></div>
+    <div className="w-16 h-16 bg-red-100"></div>
+    <div className="w-16 h-16 bg-red-100"></div>
+    <div className="w-16 h-16 bg-red-100"></div>
+    <div className="w-16 h-16 bg-red-100"></div>
+    <div className="w-16 h-16 bg-red-100"></div>
+    <div className="w-16 h-16 bg-red-100"></div>
+    <div className="w-16 h-16 bg-red-100"></div>
+    <div className="w-16 h-16 bg-red-100"></div>
+    <div className="w-16 h-16 bg-red-100"></div>
+    <div className="w-16 h-16 bg-red-100"></div>
+    <div className="w-16 h-16 bg-red-100"></div>
+    <div className="w-16 h-16 bg-red-100"></div>
+    <div className="w-16 h-16 bg-red-100"></div>
+    <div className="w-16 h-16 bg-red-100"></div>
+  </div>
+}
